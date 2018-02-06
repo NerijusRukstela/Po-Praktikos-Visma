b443a180e58f552269d8f9f575319f3181fe46bb
Teofilijus <rukstela111@gmail.com>
2018-02-06 16:30:40 +0200

    Mindei

---
+++ SeeSharp101/Basics/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeSharp101.Basics
+{
+    static class StringExtensions 
+    {
+        public static string Reverse(Graph graph)
+        {
+            string nam = "ll";
+            return nam;
+        }
+    }
+}
